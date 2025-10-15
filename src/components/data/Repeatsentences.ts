d5d92cbfb34c1e3eee1b20f035e5665e910662e5
Jefferson122 <jeffersonpinzon2121@gmail.com>
2025-10-15 11:13:07 -0400

    creando repeatsentences

---
+++ src/components/data/Repeatsentences.ts
@@ -0,0 +1,36 @@
+export interface Sentence {
+  text: string;
+  audio: string;
+}
+
+const sentences: Sentence[] = [
+  { text: "After considering all the options she decided to take the risk.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536858/repeatsentencedata/wfd1.mp3" },
+  { text: "The team have already begun their preparations for the first match of the season.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536862/repeatsentencedata/wfd2.mp3" },
+  { text: "You can choose salad or soup to your meal.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536866/repeatsentencedata/wfd3.mp3" },
+  { text: "If you like cooking, we can make supper together.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536871/repeatsentencedata/wfd4.mp3" },
+  { text: "If you need a part-time job, it should not interfere with your studies.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536874/repeatsentencedata/wfd5.mp3" },
+  { text: "The school will provide hot food.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536878/repeatsentencedata/wfd6.mp3" },
+  { text: "The weather has been lovely since this time of the year.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536882/repeatsentencedata/wfd7.mp3" },
+  { text: "Passengers on the train should take their seats.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536885/repeatsentencedata/wfd8.mp3" },
+  { text: "A public telephone is available in the shop.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536888/repeatsentencedata/wfd9.mp3" },
+  { text: "You can register your card at the university student center or the library.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536893/repeatsentencedata/wfd10.mp3" },
+  { text: "The corporation did not work well and gained no profits.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536896/repeatsentencedata/wfd11.mp3" },
+  { text: "The library will be closed except on holidays.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536899/repeatsentencedata/wfd12.mp3" },
+  { text: "You have fulfilled the enrollment requirements.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536902/repeatsentencedata/wfd13.mp3" },
+  { text: "An effective business manager is always open to new ideas.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536905/repeatsentencedata/wfd14.mp3" },
+  { text: "You can find the books in the library.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536911/repeatsentencedata/wfd15.mp3" },
+  { text: "You must call your doctor to make an appointment.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536916/repeatsentencedata/wfd16.mp3" },
+  { text: "Please close the door behind you when you leave the room.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536920/repeatsentencedata/wfd17.mp3" },
+  { text: "There has been a delay in processing the enrollment due to the computer problems.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536923/repeatsentencedata/wfd18.mp3" },
+  { text: "The old building in the university is still being used.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536927/repeatsentencedata/wfd19.mp3" },
+  { text: "If you wish to be a good student, you should attend every class.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536932/repeatsentencedata/wfd20.mp3" },
+  { text: "The bus for London will be scheduled once a week.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536935/repeatsentencedata/wfd21.mp3" },
+  { text: "You can add a salad to your meal.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536938/repeatsentencedata/wfd22.mp3" },
+  { text: "The manager will have a meeting in this room today.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536941/repeatsentencedata/wfd23.mp3" },
+  { text: "Show your passport and boarding pass at the gate.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536944/repeatsentencedata/wfd24.mp3" },
+  { text: "Different entrances to the program get the same qualification.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536947/repeatsentencedata/wfd25.mp3" },
+  { text: "You can get coffee and tea in the lunchroom.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536950/repeatsentencedata/wfd26.mp3" },
+  { text: "You can keep your bag in the back room.", audio: "https://res.cloudinary.com/ddy824awb/video/upload/v1760536953/repeatsentencedata/wfd27.mp3" },
+];
+
+export default sentences;

59edf42e1b13d3e9589fe31431c774900ac02326
Jefferson122 <jeffersonpinzon2121@gmail.com>
2025-10-15 11:16:05 -0400

    aca igual

--- src/components/shared/Speaking/RepeatSentence.tsx
+++ src/components/shared/Speaking/RepeatSentence.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import WritingDictation from "@/components/data/Repeatsentences";
+import WritingDictation from "@/components/data/repeatsentences";
 
 interface Result {
   global_score: number;

db0f4e6cc8eec2163f19dc81e7b7412fca62473d
Jefferson122 <jeffersonpinzon2121@gmail.com>
2025-10-16 13:24:14 -0400

    cambiosmks

--- src/components/shared/Speaking/RepeatSentence.tsx
+++ src/components/shared/Speaking/RepeatSentence.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import WritingDictation from "@/components/data/repeatsentences";
+import WritingDictation from "@/components/data/RepeatSenten";
 
 interface Result {
   global_score: number;

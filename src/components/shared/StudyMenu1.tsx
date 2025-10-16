c669ecb102c6a17a34825dd0194eda7bd074d9c7
Jefferson122 <jeffersonpinzon2121@gmail.com>
2025-10-16 11:20:43 -0400

    intento de dano de render

--- src/components/shared/StudyMenu1.tsx
+++ src/components/shared/StudyMenu1.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect, useRef } from "react";
 import ReadAloud from "@/components/data/ReadAloud";
 import WritingDictation from "@/components/data/WritingDictation";
-import repeatsentences from "@/components/data/repeatsentences";
+import repeatsentences from "../data/repeatsentences";
 
 
 

93348ce4b3e2e2767d0ba8670604b37cb7287750
Jefferson122 <jeffersonpinzon2121@gmail.com>
2025-11-06 14:05:19 -0500

    cambios para revisar,redireccionamiento

--- src/app/(edit)/page.tsx
+++ src/app/(edit)/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import EditForm from "./_components/EditForm";
-import "./style.scss";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function EditPage() {
+export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirige automáticamente a /post-csr cuando se carga la página principal
+    router.push("/post-csr");
+  }, [router]);
+
   return (
-    <div className="max-w-[56rem] w-full mx-auto py-10 px-6">
-      {/* Mensaje que siempre aparece */}
-      <div className="mb-6 p-4 bg-blue-100 text-blue-800 rounded">
-        Bienvenido a mi sitio web de pronunciación.
-      </div>
-
-      {/* Formulario principal */}
-      <EditForm />
-
+    <div className="loading-screen">
+      <p>Cargando...</p>
     </div>
   );
 }
